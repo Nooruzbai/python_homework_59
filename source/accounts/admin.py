409847669bc7607a47addcb6df0fe37cda918ff8
Nooruzbay <nooruzbay@gmail.com>
2022-02-21 08:37:08 +0600

    Created UserProfile

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import Profile
+
+User = get_user_model()
+
+
+class ProfileInLine(admin.StackedInline):
+    fields = ("avatar", "about_me", "git_link")
+    model = Profile
+
+
+class UserProfileAdmin(UserAdmin):
+    inlines = (ProfileInLine,)
+
+
+admin.site.unregister(User)
+admin.site.register(get_user_model(), UserProfileAdmin)
+
+
