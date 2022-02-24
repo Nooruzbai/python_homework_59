1b57fc732f7ca2d943de1faab16ba8e5f6317144
Nooruzbay <nooruzbay@gmail.com>
2022-02-24 17:34:45 +0600

    Добавьте в проект Трекер страницы редактирования личных данных и смены пароля, доступные только владельцу учётной записи.

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth.forms import UserCreationForm
+
+from accounts.models import Profile
 
 
 class MyUserCreationForm(UserCreationForm):
@@ -19,3 +21,46 @@ class MyUserCreationForm(UserCreationForm):
             raise ValidationError('Name, Last Name must be filled')
         else:
             return cleaned_data
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("email", "first_name", "last_name")
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('avatar', 'about_me', 'git_link')
+
+
+class PasswordChangeForm(forms.ModelForm):
+    password = forms.CharField(label="New Password", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Confirm Password", widget=forms.PasswordInput, strip=False)
+    old_password = forms.CharField(label="Old Password", strip=False, widget=forms.PasswordInput)
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Passwords do not match!')
+        return password_confirm
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data.get('old_password')
+        if not self.instance.check_password(old_password):
+            raise forms.ValidationError('Old password is not correct!')
+        return old_password
+
+    def save(self, commit=True):
+        user = self.instance
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+
+        return user
+
+    class Meta:
+        model = User
+        fields = ['password', 'password_confirm', 'old_password']
--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accounts.user.register import RegisterView
-from accounts.user.views import UserProfileView, UserListView
+from accounts.user.views import UserProfileView, UserListView, UpdateUserView, UserPasswordChangeView
 
 app_name = 'accounts'
 
@@ -12,5 +12,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name="registration"),
     path("profile/<int:pk>/", UserProfileView.as_view(), name="user_profile_view"),
-    path('user/list/', UserListView.as_view(), name='user_list_view')
+    path('user/list/', UserListView.as_view(), name='user_list_view'),
+    path("update/profile", UpdateUserView.as_view(), name="update_user_profile"),
+    path("change-password/", UserPasswordChangeView.as_view(), name="change_password")
 ]
